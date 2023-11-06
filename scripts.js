b7b393364d1bd9f161e592e689c4a36b29da147a
Kemar Wilson <sonic3554@gmail.com>
2023-11-05 23:50:34 -0500

    Update JS file was added with working function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-/* Add your JavaScript to this file */
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+    let submit = document.querySelectorAll(".btn")[1];
+    submit.addEventListener('click', clickHandler);
+});
+
+
+function clickHandler(event) 
+{
+    event.preventDefault();
+
+    // Get the value of the email input field
+    var emailAddress = document.getElementById("email").value;
+
+    // Select the message element
+    var messageElement = document.getElementsByClassName("message")[0];
+
+    if (emailAddress !== "") {
+        messageElement.innerHTML = "Thank You! Your email address " + emailAddress + " has been added to our mailing list.";
+    } else {
+        messageElement.innerHTML = "Please enter a valid email address";
+    }
+}
